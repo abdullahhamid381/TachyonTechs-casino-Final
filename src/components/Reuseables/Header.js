22fab1f5e9a2188d449d6ec7b33c997a18299b17
muhammadalighouri <98492348+muhammadalighouri@users.noreply.github.com>
2023-02-16 23:27:17 +0500

    update

--- src/components/Reuseables/Header.js
+++ src/components/Reuseables/Header.js
@@ -41,8 +41,6 @@ const Header = ({
                             <p><span style={{ color: "#ebebeb" }}>
                                 {/* 모든 카지노사이트를 검증하고, 카지노게임에 대한 */}
                             </span> 구체적인 베팅 노하우를 공유하는 목적으로 개설된 커뮤니티입니다. </p>
-                            <p>모든 카지노사이트를 검증하고, 카지노게임에 대한 <span style={{ color: 'white' }}>구체적인 베팅 노하우를 공유하는 목적으로 개설된 커뮤니티입니다</span>. </p>
-                            <p><span style={{ color: "#ebebeb" }}>모든 카지노사이트를 검증하고, 카지노게임에 대한</span> 구체적인 베팅 노하우를 공유하는 목적으로 개설된 커뮤니티입니다. </p>
                         </div>
                         <div className="end">
                             <div className="links">
